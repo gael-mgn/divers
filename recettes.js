16b8ead028e852e70a50a2f952ab20f5aa80083e
gael-mgn <gael.maignan@eivp-paris.fr>
2025-07-28 18:45:43 +0200

    Add files via upload

--- recettes.js
+++ recettes.js
@@ -18,13 +18,122 @@ const recettes = {
       lien: "https://terredamandes.fr/recette/chouchou-aux-amandes/"
     }
   ],
-  "Chèvrefeuille": [
+  "Amélanchier": [
     {
-      titre: "Muffins aux baies et au muesli",
-      image: "https://www.holle.ch/wp-content/uploads/2024/08/Beeren-Muesli-Muffins_Rezeot.png",
-      lien: "https://www.holle.ch/fr/recettes/muffins-aux-baies-et-au-muesli/"
+      titre: "Chouchous aux amandes",
+      image: "https://isabellecotenutritionniste.com/wp-content/uploads/2018/07/Compote-amelanche-haut-petit-980x749.jpg",
+      lien: "https://isabellecotenutritionniste.com/recettes/compote-damelanches/"
     }
   ],
+  "Arbousier": [
+    {
+      titre: "Confiture d’arbouse",
+      image: "https://img.cuisineaz.com/660x495/2016/03/17/i21778-confiture-d-arbouses.webp",
+      lien: "https://www.cuisineaz.com/recettes/confiture-d-arbouses-31178.aspx"
+    }
+  ],
+  "Argousier": [
+    {
+      titre: "Sauce à l'argousier, érable et romarin",
+      image: "https://www.argousiere.com/wp-content/uploads/2018/11/sauce-argousier-romarin-et-erable-833x800-1000x600-1-600x576.jpg",
+      lien: "https://www.argousiere.com/recipe/sauce-a-largousier-erable-et-romarin/"
+    }
+  ],
+  "Aronie": [
+    {
+      titre: "Boulettes de viande aux baies d’aronie",
+      image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiM3IbRSUdQu4oIEI7dV8q339BLlxxhEPDrSEZKE6nje6Wff7_YIziYKCcrTG0Qr6djkPVaqZ9LnHpVKpbttUVntobd_NX8Y8i44J1a8wreHqPIRpBsKPwTKe4xhZpuvRt0MlDY0MNPDEA/s640/boulettes+baies+d%2527aronia.jpg",
+      lien: "https://www.un-peu-gay-dans-les-coings.eu/2018/10/boulettes-de-viande-aux-baies-daronia.html"
+    }
+  ],
+
+
+  "Aubépine": [
+    {
+      titre: "Gelée de baie d’aubépine",
+      image: "https://img-global.cpcdn.com/recipes/c2a8ed4d2c7a0b05/600x852cq80/gelee-de-baies-daubepine-photo-principale-de-la-recette.webp",
+      lien: "https://cookpad.com/fr/recettes/16541175"
+    }
+  ],
+  "Aubépine": [
+    {
+      titre: "Gelée de baie d’aubépine",
+      image: "https://img-global.cpcdn.com/recipes/c2a8ed4d2c7a0b05/600x852cq80/gelee-de-baies-daubepine-photo-principale-de-la-recette.webp",
+      lien: "https://cookpad.com/fr/recettes/16541175"
+    }
+  ],
+
+
+
+  "Cerisier à fruits": [
+    {
+      titre: "Vin de feuille de cerisier",
+      image: "https://lagrangeauxsavoirfaire.fr/wp-content/uploads/2021/01/la-grange-aux-savoir-faire-recette-du-guignolet-11.jpg",
+      lien: "https://lagrangeauxsavoirfaire.fr/blog/recettes/recette_guignolet/"
+    },
+    {
+      titre: "Riz au fleur de cerisier",
+      image: "https://img.lemde.fr/2023/08/17/0/0/1500/1200/960/0/75/0/098c931_297811-3334969.jpg",
+      lien: "https://www.lemonde.fr/les-recettes-du-monde/article/2023/08/25/le-riz-aux-fleurs-de-cerisier-la-recette-de-nancy-singleton-hachisu_6186559_5324493.html"
+    }
+  ],
+  "Châtaignier": [
+    {
+      titre: "Velouté de châtaigne",
+      image: "",
+      lien: "https://www.lesfruitsetlegumesfrais.com/recettes/veloute-de-chataigne"
+    }
+  ],
+  "Chèvrefeuille": [
+    {
+      titre: "Rhum chèvrefeuille",
+      image: "https://www.rhum-arrange.fr/recettes/rhum-arrange/rhum-chevrefeuille/",
+      lien: "https://www.rhum-arrange.fr/wp-content/uploads/2008/06/fleurs-de-chevrefeuilles-scaled.jpg"
+    }
+  ],
+
+  "Cognassier": [
+    {
+      titre: "Poêlée de coing et magret au four",
+      image: "",
+      lien: "https://www.lesfruitsetlegumesfrais.com/recettes/poelee-de-coing-et-magret-au-four"
+    },
+    {
+      titre: "Pâte de coings",
+      image: "",
+      lien: "https://cuisine.journaldesfemmes.fr/recette/305270-pate-de-coings"
+    }
+  ],
+   "Cornouiller": [
+    {
+      titre: "Cornouiller mariné",
+      image: "https://taste.decorexpro.com/images/article/thumb/715-0/2018/10/kak-zamarinovat-kizil-1.jpg",
+      lien: "https://taste.decorexpro.com/kizil/kak-zamarinovat/"
+    },
+    {
+      titre: "Chutney de cornouilles",
+      image: "https://img-3.journaldesfemmes.fr/R1mQxLA7cDZgwGwphDmFZwoTuPQ=/750x500/7b983877925742028c4135c74dc2f5b7/ccmcms-jdf/39917012.jpg",
+      lien: "https://cuisine.journaldesfemmes.fr/recette/318758-chutney-de-cornouilles"
+    }
+  ],
+  "Figuier": [
+    {
+      titre: "Saucisson au chocolat et aux figues",
+      image: "https://content.joseedistasio.ca/app/uploads/2020/11/30151610/saucisson-chocolat-349-999x1501.jpg",
+      lien: "https://www.joseedistasio.ca/recettes/saucisson-au-chocolat-et-aux-figues/"
+    }
+  ],
+  "Glycine": [
+    {
+      titre: "Beignet de fleur de glycine",
+      image: "https://www.julieandrieu.com/media/cache/web_recipe_detail/uploads/recettes/dessert/beignets-fleurs-glycines-camille-oger.jpg",
+      lien: "https://www.julieandrieu.com/recettes/beignets-fleurs-glycines-camille-oger"
+    }
+  ],
+
+
+
+
   "Tilleul": [
     {
       titre: "Infusion de fleurs de tilleul",
